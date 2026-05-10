7ed5ef9b4ecea0145f7ceac0debcc6304ee9812f
Joseph Guochen Wan <jgw227@cornell.edu>
2026-05-10 16:50:19 -0400

    compile

--- DE1-SoC_Project/verilog/Computer_System/synthesis/Computer_System.v
+++ DE1-SoC_Project/verilog/Computer_System/synthesis/Computer_System.v
@@ -603,8 +603,8 @@ module Computer_System (
 	);
 
 	mcmc_system_parallel #(
-		.N_NODES  (32),
-		.N_CHAINS (1)
+		.N_NODES  (27),
+		.N_CHAINS (4)
 	) mcmc_system_0 (
 		.clk           (system_pll_sys_clk_clk),                                   //          clock.clk
 		.reset_n       (~rst_controller_reset_out_reset),                          //          reset.reset_n
